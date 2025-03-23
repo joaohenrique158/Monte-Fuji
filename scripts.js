9414b3e756cabc653c6749a146c44245c4f0862a
joaohenrique158 <joaohenriqueaquino10@gmail.com>
2025-03-23 16:13:12 -0400

    Alteração das imagens das casas 1, 2, 3, 4 e 5. Criação do catálogo de imóveis

--- scripts.js
+++ scripts.js
@@ -29,3 +29,29 @@ ScrollReveal().reveal(".header__container h1",{
     ...scrollRevealOption,
     delay: 500,
 });
+
+// about container
+ScrollReveal().reveal(".about__image img",{
+    ...scrollRevealOption,
+    origin: "left",
+});
+
+ScrollReveal().reveal(".about__content .section__subheader",{
+    ...scrollRevealOption,
+    delay: 500,
+});
+
+ScrollReveal().reveal(".about__content .section__header",{
+    ...scrollRevealOption,
+    delay: 1000,
+});
+
+ScrollReveal().reveal(".about__content .section__description",{
+    ...scrollRevealOption,
+    delay: 1500,
+});
+
+ScrollReveal().reveal(".about__btn",{
+    ...scrollRevealOption,
+    delay: 2000,
+});
